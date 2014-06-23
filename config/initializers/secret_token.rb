e5eee422114364f845e6e6a72474258eaa166dae
Dietrich Mensah <dietricherwane@yahoo.fr>
2014-06-23 10:48:43 +0000

    changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HubsFrontOffice::Application.config.secret_key_base = '537685f1edb34c7f58bd5471dccf489124089c1169a4f3fdb78f228bfa4ad3f3f0e59b799080491f1b2b721b94d288cf740f8a6d4b83f28dacebf09c2f14cf3d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HubsFrontOffice::Application.config.session_store :cookie_store, key: '_HubsFrontOffice_session'

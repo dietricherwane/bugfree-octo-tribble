8290b1e2225c1108c74950d1c90b1234a69c9e20
nsia <erwan@ubuntu.(none)>
2014-07-08 14:29:20 +0000

    changes

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module HubsFrontOffice
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :fr
+    I18n.enforce_available_locales = false
   end
 end
